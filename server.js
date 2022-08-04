00c94041d8316a8ec945e77e1a5308c6f719c67d
ASafronov <asafronov@sportmaster.ru>
2022-08-04 08:47:40 +0300

    express

--- server.js
+++ server.js
@@ -1,100 +1,81 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-
-
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-
-class MyEmitter extends EventEmitter {
-}
-
-
-const myEmitter = new MyEmitter();
-myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
-
+const cors = require('cors')
+const {logger} = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500;
 
-const serveFile = async (filePath, contentType, response) => {
-    try {
-        const rawData = await fsPromises.readFile(
-            filePath,
-            !contentType.includes('image') ? 'utf8' : ''
-        );
-        const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData;
-        response.writeHead(
-            filePath.includes('404.html') ? 404 : 200,
-            {'Content-Type': contentType}
-        );
-        response.end(contentType === 'application/json' ? JSON.stringify(data) : data);
-    } catch (err) {
-        console.log(err);
-        myEmitter.emit('log', `${err.name}\t${err.message}`, 'errLog.txt');
-        response.statusCode = 500;
-        response.end();
-    }
-}
+app.use(logger);
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
-
-    const extension = req.url === '/'? '.html': path.extname(req.url);
-    let contentType;
-    let filePath;
-    switch (extension) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        default:
-            contentType = 'text/html';
-            break;
-    }
-
-    switch (extension) {
-        case '.html':
-            if (req.url === '/')
-                filePath = path.join(__dirname, 'views', 'index.html');
-            else if (req.url.slice(-1) === '/')
-                filePath = path.join(__dirname, 'views', req.url, 'index.html');
-            else
-                filePath = path.join(__dirname, 'views', req.url);
-            break;
-        default:
-            filePath = path.join(__dirname, req.url);
-    }
-    if (!extension && req.url.slice(-1) === '/') filePath += ".html";
-
-    const fileExists = fs.existsSync(filePath);
-
-    if (fileExists) {
-        serveFile(filePath, contentType, res);
-    } else {
-        switch (path.parse(filePath).base) {
-            case 'old-page.html':
-                res.writeHead(301, {'Location': '/new-page.html'});
-                res.end();
-                break;
-            case 'www-page.html':
-                res.writeHead(301, {'Location': '/'});
-                res.end();
-                break;
-            default:
-                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+//Cross origin resource sharing
+const whitelist = ['https://www.google.ru1', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed CORS'));
         }
-    }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('^/$|index(.html)?', (req, res) => {
+    //res.sendFile('./views/index.html', {root: __dirname});
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirected(301, '/new-page.html'); //302 by default
+});
+
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}
+const three = (req, res) => {
+    console.log('three');
+    res.send('Finished!');
+}
+
+app.get('/chain(.html)?', [one, two, three]);
+
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next()
+}, (req, res) => {
+    res.send('Hello world!');
+});
+
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({error: "404 Not Found"});
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
