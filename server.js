aa098985f37bd497964246d73d40801fc39cb1bd
ASafronov <asafronov@sportmaster.ru>
2022-08-04 12:27:41 +0300

    Router

--- server.js
+++ server.js
@@ -24,43 +24,16 @@ app.use(cors(corsOptions));
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/subdir', express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
 
-app.get('^/$|index(.html)?', (req, res) => {
-    //res.sendFile('./views/index.html', {root: __dirname});
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
 
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirected(301, '/new-page.html'); //302 by default
-});
-
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
-
-app.get('/chain(.html)?', [one, two, three]);
-
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello world!');
-});
 
 
 app.all('*', (req, res) => {
