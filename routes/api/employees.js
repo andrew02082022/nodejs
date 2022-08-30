2440c649d81b39495ba87a07d4c6bcd6b5c25025
ASafronov <asafronov@sportmaster.ru>
2022-08-30 14:38:40 +0300

    Создание механизма аутентификации пользователя

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getAllEmployees, createNewEployee, updateEmployee, deleteEmployee, getEmployee} = require("../../controllers/employeesControler");
+const {getAllEmployees, createNewEployee, updateEmployee, deleteEmployee, getEmployee} = require("../../controllers/employeesController");
 
 router.route('/')
     .get(getAllEmployees)
